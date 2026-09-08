656cc06a0b42b5cfdc7d9bc93d75433dfab6d327
oakdencarl-ship-it <oakdencarl@gmail.com>
2026-09-08 12:02:28 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: 'window',
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }
+})
